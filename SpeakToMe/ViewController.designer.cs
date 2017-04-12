1b9cb4ca7e5768097e065c8a78a8e1664e4a551f
Suresh Balla <suresh.balla@outlook.com>
2017-04-12 22:32:47 +0530

    update ios app icon - fixed issues with image references in project file

--- SpeakToMe/ViewController.designer.cs
+++ SpeakToMe/ViewController.designer.cs
@@ -1,53 +1,68 @@
 // WARNING
 //
-// This file has been generated automatically by Xamarin Studio to store outlets and
-// actions made in the UI designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
 using Foundation;
+using System;
 using System.CodeDom.Compiler;
 
 namespace SpeakToMe
 {
-	[Register ("ViewController")]
-	partial class ViewController
-	{
-		[Outlet]
-		UIKit.UIPickerView pickerView { get; set; }
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        UIKit.UIPickerView pickerView { get; set; }
 
-		[Outlet]
-		UIKit.UITextView recordStatus { get; set; }
 
-		[Outlet]
-		UIKit.UITextView selectedScreenTextView { get; set; }
+        [Outlet]
+        UIKit.UITextView recordStatus { get; set; }
 
-		[Outlet]
-		UIKit.UIButton selectScreenButton { get; set; }
 
-		[Action ("selectScreenButton_Click:")]
-		partial void selectScreenButton_Click (Foundation.NSObject sender);
-		
-		void ReleaseDesignerOutlets ()
-		{
-			if (pickerView != null) {
-				pickerView.Dispose ();
-				pickerView = null;
-			}
+        [Outlet]
+        UIKit.UITextView selectedScreenTextView { get; set; }
 
-			if (selectedScreenTextView != null) {
-				selectedScreenTextView.Dispose ();
-				selectedScreenTextView = null;
-			}
 
-			if (selectScreenButton != null) {
-				selectScreenButton.Dispose ();
-				selectScreenButton = null;
-			}
+        [Outlet]
+        UIKit.UIButton selectScreenButton { get; set; }
 
-			if (recordStatus != null) {
-				recordStatus.Dispose ();
-				recordStatus = null;
-			}
-		}
-	}
+
+        [Action ("selectScreenButton_Click:")]
+        partial void selectScreenButton_Click (Foundation.NSObject sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (pickerView != null) {
+                pickerView.Dispose ();
+                pickerView = null;
+            }
+
+            if (recordButton != null) {
+                recordButton.Dispose ();
+                recordButton = null;
+            }
+
+            if (recordStatus != null) {
+                recordStatus.Dispose ();
+                recordStatus = null;
+            }
+
+            if (selectedScreenTextView != null) {
+                selectedScreenTextView.Dispose ();
+                selectedScreenTextView = null;
+            }
+
+            if (selectScreenButton != null) {
+                selectScreenButton.Dispose ();
+                selectScreenButton = null;
+            }
+
+            if (textView != null) {
+                textView.Dispose ();
+                textView = null;
+            }
+        }
+    }
 }
